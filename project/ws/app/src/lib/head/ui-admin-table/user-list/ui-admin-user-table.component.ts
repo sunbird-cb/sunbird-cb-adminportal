c0f34c328309c89ae3f093f9f88160fabb76ecee
garima468 <garima.chaudhary@tarento.com>
2023-06-21 23:19:35 +0530

    lint fixes

--- project/ws/app/src/lib/head/ui-admin-table/user-list/ui-admin-user-table.component.ts
+++ project/ws/app/src/lib/head/ui-admin-table/user-list/ui-admin-user-table.component.ts
@@ -185,10 +185,9 @@ export class UIAdminUserTableComponent implements OnInit, AfterViewInit, OnChang
                   this.snackBar.open('Admin assigned Successfully')
                   this.router.navigate(['/app/home/directory', { department: this.departmentRole }])
                 }
-              },
-                (err: { error: any }) => {
-                  this.openSnackbar(err.error.message)
-                })
+              }, (err: { error: any }) => {
+                this.openSnackbar(err.error.message)
+              })
             }
           }
         })
